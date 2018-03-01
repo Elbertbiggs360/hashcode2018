90167e9808c15242ce236f0a83d04689fc49f42a
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-03-02 00:06:56 +0300

    Add brainiac file to handle functionality Add coordinate moving functions to Vahicle class

---
+++ brainiac.py
@@ -0,0 +1,5 @@
+class Brainiac():
+    ''' Class to handle sorting of arrays'''
+
+    def __init__(self):
+        self.array = [];
--- vehicle.py
+++ vehicle.py
@@ -1,3 +1,9 @@
 class Vehicle(object):
     def __init__(self, x, y):
         self.pos = [x, y]
+
+    def moveOneX():
+        return self.pos[x]++
+
+    def moveOneY(y_coord):
+        return self.pos[y]++
